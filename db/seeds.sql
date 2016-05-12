9a334ba4207e7352544a9634f36c78b5d6984a2e
Nicole Brower <seow.nicole86@gmail.com>
2016-05-11 23:43:15 -0400

    needs better css

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken burger');
+INSERT INTO burgers (burger_name) VALUES ('Pulled pork burger');
+INSERT INTO burgers (burger_name) VALUES ('Mac and cheese burger');
